6108ebafc1ae34a2ad612be2f4eddf64658f3161
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-08 08:09:48 -0300

    Adicionar a recuperação de organizações na rota de organizações

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -26,21 +27,26 @@ app.use(express.static(path.join(__dirname, 'public')));
   */
 app.get('/', async (req, res) => {
     const title = 'Home';
+
     res.render('home', { title });
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
+
     res.render('projects', { title });
 });
 
 app.get('/categories', async (req, res) => {
     const title = 'Service Categories';
+
     res.render('categories', { title });
 });
 
