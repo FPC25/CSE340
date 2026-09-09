101700ed58bf274f2e49a4f19423dfe44f202d9f
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-09 20:55:47 -0300

    Adicionar tratamento de erro na função getAllProjects

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,22 +1,28 @@
 import db from './db.js'
 
 const getAllProjects = async() => {
-    const query = `
-        SELECT 
-            p.organization_id, 
-            p.title, 
-            p.description, 
-            p.location, 
-            TO_CHAR(p.date, 'DD/MM/YYYY') AS date, 
-            o.orgname
-        FROM project p
-        JOIN organization o ON o.organization_id = p.organization_id
-        Order by date ASC; 
-    `;
+    try {
+        const query = `
+            SELECT 
+                p.organization_id, 
+                p.title, 
+                p.description, 
+                p.location, 
+                TO_CHAR(p.date, 'DD/MM/YYYY') AS date, 
+                o.orgname
+            FROM project p
+            JOIN organization o ON o.organization_id = p.organization_id
+            Order by date ASC; 
+        `;
 
-    const result = await db.query(query);
+        const result = await db.query(query);
 
-    return result.rows;
+        return result.rows;
+    }
+    catch (error) {
+        console.error('Error fetching projects:', error);
+        throw error;
+    }
 }
 
 export { getAllProjects } 
