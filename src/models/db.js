a8929c5ceb616eb23ce5624dde7bab833582e11c
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-09 20:58:12 -0300

    Atualizar configuração SSL do pool de conexão para habilitar apenas em desenvolvimento

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: process.env.NODE_ENV === 'development' // SSL enabled in development, disabled in production.
 });
 
 /**
