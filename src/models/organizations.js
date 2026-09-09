6e963869fd8422d0563de9623cd591aba637a56b
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-09 20:55:41 -0300

    Adicionar tratamento de erro na função getAllOrganizations

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,14 +1,19 @@
 import db from './db.js'
 
 const getAllOrganizations = async() => {
+  try {
     const query = `
-        SELECT organization_id, orgname, description, contact_email, logo_filename
+        SELECT orgname, description, contact_email, logo_filename
       FROM public.organization;
     `;
 
     const result = await db.query(query);
 
     return result.rows;
+  } catch (error) {
+    console.error('Error fetching organizations:', error);
+    throw error;
+  }
 }
 
 export {getAllOrganizations}  
