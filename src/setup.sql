5d435f892eaeefcb43d8975a034e6d6ba91933e4
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-10 22:10:19 -0300

    Adicionar tabelas de categoria e projeto_categoria, e atualizar a tabela de projeto para remover a referência obrigatória à organização

--- src/setup.sql
+++ src/setup.sql
@@ -14,7 +14,7 @@ values
 
 CREATE TABLE project (
 	"project_id" serial primary key,
-	"organization_id" int references organization(organization_id) not null,
+	"organization_id" int not null,
 	"title" varchar(150) not null,
 	"description" text not null,
 	"location" varchar(255) not null,
@@ -46,3 +46,58 @@ INSERT INTO project ("organization_id", "title", "description", "location", "dat
 (3, 'After-School Tutoring Support', 'Assisting elementary students with math and reading.', 'Eastside Library', '2026-10-25'),
 (3, 'Coat & Blanket Collection', 'Sorting donated winter gear for shelter distribution.', 'Community Warehouse', '2026-11-05'),
 (3, 'Holiday Care Package Packing', 'Assembling care boxes for veterans and seniors.', 'Unity Hall', '2026-11-25');
+
+CREATE TABLE category (
+	category_id serial primary key,
+	category_name varchar(255) not null
+);
+
+CREATE TABLE project_category (
+	project_id int not null,
+	category_id int not null,
+
+	CONSTRAINT pk_project_category
+		PRIMARY KEY (project_id, category_id),
+		
+	CONSTRAINT fk_project
+		FOREIGN KEY (project_id)
+		REFERENCES project(project_id)
+		ON DELETE CASCADE,
+		
+	CONSTRAINT fk_category
+		FOREIGN KEY (category_id)
+		REFERENCES category(category_id)
+		ON DELETE CASCADE
+);
+
+INSERT INTO category(category_name) VALUES
+('Construction and Housing'),
+('Environment and Sustainability'),
+('Food Security'),
+('Education and Tutoring'),
+('Community Support'),
+('Accessibility'),
+('Senior Support');
+
+INSERT INTO project_category (project_id, category_id) VALUES 
+-- Projetos da BrightFuture Builders (project_id 1 a 5) 
+(1, 1), (1, 5), 
+(2, 1), (2, 5), 
+(3, 1), (3, 5), 
+(4, 1), (4, 5), (4, 6), (4, 7), 
+(5, 1), (5, 5), (5, 7), 
+
+-- Projetos da GreenHarvest Growers (project_id 6 a 10) 
+(6, 2), (6, 3),	(6, 5), 
+(7, 1), (7, 2), 
+(8, 2), (8, 3), (8, 4), 
+(9, 1), (9, 2), (9, 4), 
+(10, 3), (10, 5), 
+
+-- Projetos da UnityServe Volunteers (project_id 11 a 15) 
+(11, 3), (11, 5),
+(12, 2), (12, 5), 
+(13, 4), (13, 5), 
+(14, 5), 
+(15, 5), (15, 7);
+
