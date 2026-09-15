d7056a879040299dcfa2a343178ee90dc9c6dffc
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-15 20:33:54 -0300

    Refatorar configuração do middleware e ajustes na configuração do EJS

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
 /**
   * Routes
   */
@@ -53,12 +67,6 @@ app.get('/categories', async (req, res) => {
 
     res.render('categories', { title, categories });
 });
-
-// Set EJS as the templating engine
-app.set('view engine', 'ejs');
-
-// Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
 
 app.listen(PORT, async () => {
     try{
