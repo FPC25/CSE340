cef831e254749841ca661c9272d60eadc087628f
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-16 15:10:16 -0300

    Adiciona consulta para obter detalhes da organização e atualiza a função de listar organizações

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -3,7 +3,8 @@ import db from './db.js'
 const getAllOrganizations = async() => {
   try {
     const query = `
-        SELECT orgname, description, contact_email, logo_filename
+        SELECT 
+        organization_id, orgname, description, contact_email, logo_filename
       FROM public.organization;
     `;
 
@@ -14,6 +15,20 @@ const getAllOrganizations = async() => {
     console.error('Error fetching organizations:', error);
     throw error;
   }
-}
+};
 
-export {getAllOrganizations}  
+const getOrganizationDetails = async (organizationId) => {
+    const query = `
+      SELECT organization_id, orgname, description, contact_email, logo_filename
+      FROM organization
+      WHERE organization_id = $1;
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    // Return the first row of the result set, or null if no rows are  found
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+export {getAllOrganizations, getOrganizationDetails}; 
