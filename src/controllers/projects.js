d1dfbf6b944ebde77f9d9ecda6f8b0dbc86a15f4
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-19 10:15:17 -0300

    Refatora funções de controle para melhor legibilidade e adiciona categorias aos detalhes do projeto

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,22 +1,36 @@
 // Import any needed model functions
-import { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails } from '../models/projects.js';
+import { 
+    getUpcomingProjects, 
+    getProjectDetails 
+} from '../models/projects.js';
+
+import { getCategoriesByProjectsId } from '../models/categories.js';
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
-    NUMBER_OF_UPCOMING_PROJECTS = 5; // Define the number of upcoming projects to display
+    const NUMBER_OF_UPCOMING_PROJECTS = 5; // Define the number of upcoming projects to display
     const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
     const title = 'Upcoming Projects';
 
-    res.render('projects', { title, projects });
+    res.render('projects', { 
+        title, 
+        projects 
+    });
 };
 
 const showProjectDetailsPage = async (req, res) => {
     const projectId = req.params.id;
+
     const projectDetails= await getProjectDetails(projectId);
+    const categoriesForProject = await getCategoriesByProjectsId(projectId);
 
-    const title = projectDetails.title; // Assuming getProjectDetails returns the title as the second element
+    const title = projectDetails.title; 
 
-    res.render('project', { title, projectDetails });
+    res.render('project', { 
+        title, 
+        projectDetails, 
+        categoriesForProject 
+    });
 }
 
 // Export any controller functions
