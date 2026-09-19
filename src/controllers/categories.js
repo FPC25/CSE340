00d74e84b42195b970ddbf4a484512d33b696cd9
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-19 10:03:29 -0300

    Adiciona função para exibir detalhes da categoria e ajusta a formatação do código

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,13 +1,37 @@
 // Import any needed model functions
-import { getAllCategories } from '../models/categories.js';
+import { 
+    getAllCategories, 
+    getCategoryDetails } 
+from '../models/categories.js';
+
+import { getProjectsByCategoryId } from '../models/projects.js';
+
 
 // Define any controller functions
 const showCategoriesPage = async (req, res) => {
     const categories = await getAllCategories();
     const title = 'Service Categories';
 
-    res.render('categories', { title, categories });
-};  
+    res.render('categories', { 
+        title, 
+        categories 
+    });
+}; 
+
+const showCategoryDetailsPage = async (req, res) => {
+    const categoryId = req.params.id;
+    
+    const categoryDetails = await getCategoryDetails(categoryId);
+    const projectsInCategory = await getProjectsByCategoryId(categoryId);
+
+    const title = categoryDetails.category_name;
+
+    res.render('category-details', { 
+        title, 
+        categoryDetails, 
+        projectsInCategory 
+    });
+};
 
 // Export any controller functions
-export { showCategoriesPage };
+export { showCategoriesPage, showCategoryDetailsPage };
