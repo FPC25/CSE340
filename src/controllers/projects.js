616554cf1045b6cbd9fe54edfd95765546171236
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-09-23 23:19:20 -0300

    adiciona funções para criar e processar novos projetos no formulário

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -2,6 +2,7 @@
 import { 
     getUpcomingProjects, 
     getProjectDetails,
+    createProject,
     updateProject
 } from '../models/projects.js';
 
@@ -10,6 +11,40 @@ import { getCategoriesByProjectsId } from '../models/categories.js';
 import { getAllOrganizations } from '../models/organizations.js';
 
 import { body, validationResult } from 'express-validator';
+
+// Define validation and sanitization rules for project form
+// Define validation rules for project form
+const projectValidation = [
+    body('title')
+        .trim()
+        .notEmpty()
+        .withMessage('Project title is required')
+        .isLength({min: 3, max: 200})
+        .withMessage('Project title must be between 3 and 200 characters'),
+    body('description')
+        .trim()
+        .notEmpty()
+        .withMessage('Project description is required')
+        .isLength({ max: 1000 })
+        .withMessage('Project description cannot exceed 1000 characters'),
+    body('location')
+        .trim()
+        .notEmpty()
+        .withMessage('Project location is required')
+        .isLength({ max: 200 })
+        .withMessage('Project location must be less than 200 characters'),
+    body('date')
+        .trim()
+        .notEmpty()
+        .withMessage('Project date is required')
+        .isISO8601()
+        .withMessage('Date must be a valid date format'),
+    body('organization_id')
+        .notEmpty()
+        .withMessage('Organization Id is required')
+        .isInt()
+        .withMessage('Organization must be a valid integer')
+]
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
@@ -36,6 +71,44 @@ const showProjectDetailsPage = async (req, res) => {
         projectDetails, 
         categoriesForProject 
     });
+}
+
+const showNewProjectForm = async (req, res) => {
+    const allOrganizations = await getAllOrganizations();
+
+    const title = 'Create Project';
+
+    res.render('new-project', { title, allOrganizations })
+}
+
+const processNewProjectForm = async (req, res) => {
+    // Check for validation errors
+    const results = validationResult(req);
+
+    if (!results.isEmpty()) {
+        results.array().forEach((error) => {
+            req.flash('error', error.msg);
+        });
+
+        return res.redirect('/new-project');
+    }
+
+    const { organization_id, title, description, location, date } = req.body;
+ 
+
+    try {
+        const newProjectId = await createProject(organization_id, title, description, location, date);
+
+        // Set a success flash message
+        req.flash('success', 'Project created successfully!');
+
+        res.redirect(`/project/${newProjectId}`);
+    } catch (error) {
+        console.error('Error creating new project:', error);
+        req.flash('error', 'There was an error creating the service project.');
+        res.redirect('/new-project');
+    }
+    
 }
 
 const showEditProjectForm = async (req, res) => {
@@ -72,4 +145,12 @@ const processEditProjectForm = async (req, res) => {
 }
 
 // Export any controller functions
-export { showProjectsPage, showProjectDetailsPage, showEditProjectForm, processEditProjectForm };
+export { 
+    showProjectsPage, 
+    showProjectDetailsPage, 
+    showNewProjectForm,
+    processNewProjectForm,
+    showEditProjectForm, 
+    processEditProjectForm, 
+    projectValidation
+};
